52e0b7e6001c218825000b00919f3131b936f354
uday4all <mininguday@gmail.com>
2017-02-21 13:26:43 +0530

    commented you are logged and non-logged code space area

--- functions/wezee/global_functions.php
+++ functions/wezee/global_functions.php
@@ -6,10 +6,10 @@ function check_if_logged(){
 
 $user_authentication_obj = new login_authenticator();
 if($user_authentication_obj->islogged($_SERVER['PHP_SELF'])) {
-echo  'you are signed in';
+//echo  'you are signed in';
 }
 else{
-echo  'you are not logged';
+//echo  'you are not logged';
 
 }
 }
